1724e35c5631a9375815db97e97ddaed32192b2d
DavidRoszina <david.a.roszina@gmail.com>
2020-05-19 11:56:42 -0500

    Added use  of ApplicationDbContext inside controller.

--- SuperHeroesWeb/Data/ApplicationDbContext.cs
+++ SuperHeroesWeb/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHeroesWeb.Models;
 
 namespace SuperHeroesWeb.Data
 {
@@ -12,5 +13,6 @@ namespace SuperHeroesWeb.Data
             : base(options)
         {
         }
+        public DbSet<SuperHero> SuperHeroes { get; set; } 
     }
 }
--- SuperHeroesWeb/Models/SuperHero.cs
+++ SuperHeroesWeb/Models/SuperHero.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace SuperHeroesWeb.Models
 {
     public class SuperHero
+        
     {
         [Key]
         public int Id { get; set; }
